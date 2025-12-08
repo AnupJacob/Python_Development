9597bd01649cafb24ae5563b7c0f57cda7bd5267
anupjacob <anup.jacob@outlook.com>
2025-12-08 10:09:45 +0000

    Adding codes for reverse number functionality

--- Reversenumber.py
+++ Reversenumber.py
@@ -9,9 +9,23 @@
 # Licensing     : Anup Jacob, DevOps
 # ----------------------------------
 
+def modoper(num):
+    modnum = num%10
+    return modnum
+
+def reversenum(numinput):
+    result = 0
+
+    lennum = len(str(numinput))
+    print("Length of the number ",numinput," is ",lennum)
+    for i in range(0,lennum):
+        result = result*10 + modoper(numinput)
+        numinput = numinput // 10
+
+    return result
 
 if __name__ == '__main__':
 
     numinput = int(input("Enter the number to be reversed :"))
 
-    print("Reverse of the number ",numinput," is :")
+    print("Reverse of the number ",numinput," is :",reversenum(numinput))
