ee5f40e4e8f14d020a39ba74ce16606bb169510d
anupjacob <anup.jacob@outlook.com>
2025-12-08 09:52:59 +0000

    Initial draft of reverse number

---
+++ Reversenumber.py
@@ -0,0 +1,17 @@
+# ---------------------------------
+
+# File          : Reversenumber.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 08/12/2025
+# Modified Date : 08/12/2025
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+
+if __name__ == '__main__':
+
+    numinput = int(input("Enter the number to be reversed :"))
+
+    print("Reverse of the number ",numinput," is :")
