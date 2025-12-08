f02c4200d0f3ad2fe151532c5ac634786eebe8dc
anupjacob <anup.jacob@outlook.com>
2025-12-08 12:39:39 +0000

    Adding codes for lambdatest functionality

---
+++ lambdatest.py
@@ -0,0 +1,17 @@
+# ---------------------------------
+
+# File          : lambdatest.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 08/12/2025
+# Modified Date : 08/12/2025
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+x = int(input("Enter the first number : "))
+y = int(input("Enter the second number :"))
+
+n = lambda x,y:x * y
+
+print(n(x,y))
