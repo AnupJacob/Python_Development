cedb56f3a460abe8b63569f5fbbee59bd6524e32
AnupJacob <anupjacob2017@gmail.com>
2025-12-19 14:05:47 +0000

    draft max consecutive ones problem

---
+++ leetcodes/max-consecutive-ones.py
@@ -0,0 +1,37 @@
+# ---------------------------------
+
+# File          : max-consecutive-ones
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 19/12/2025
+# Modified Date : 19/12/2025
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+'''
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+
+'''
+
+def max_consecutive_ones():
+
+
+if __name__ == '__main__':
+    max_consecutive_ones()
