d072e1a25019b1d6b5d12e13a31cfc8fb45aee2d
AnupJacob <anupjacob2017@gmail.com>
2025-12-29 12:14:26 +0000

    reverse word test

---
+++ reverseword.py
@@ -0,0 +1,27 @@
+# ---------------------------------
+
+# File          : reverseword.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 29/12/2025
+# Modified Date : 29/12/2025
+# Description   :
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
+
+
+if __name__ == '__main__':
+
+    word = input("Enter the word to be reversed: ")
+    wordlen = len(word)
+    print("The length of the word is ",wordlen)
+    print("The word to be reversed is", word)
+    reverseword = []
+    for i in range(wordlen,0,-1):
+        print(word[i-1])
+        reverseword.append(word[i-1])
+
+    reverseword = ''.join(reverseword)
+    revword2 = ''.join(reversed(word))
+    print("The reverse of the word is ", reverseword)
+    print("The reverse operation by function gives: ", revword2)
