5ea628dc1d592aa59fba2cfd8fa522c5ee2c6690
AnupJacob <anupjacob2017@gmail.com>
2025-12-29 13:20:07 +0000

    Initial commit for practice recursive function

---
+++ recursivepgm.py
@@ -0,0 +1,11 @@
+# ---------------------------------
+
+# File          : recursivepgm.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 29/12/2025
+# Modified Date : 29/12/2025
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
