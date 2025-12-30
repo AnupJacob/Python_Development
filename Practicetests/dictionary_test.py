e7af3cab13cdeb1b93088e5fbee893050d3f628d
AnupJacob <anupjacob2017@gmail.com>
2025-12-30 14:53:16 +0000

    Updated manual dictionary elements addition program test

--- Practicetests/dictionary_test.py
+++ Practicetests/dictionary_test.py
@@ -9,8 +9,22 @@
 # Licensing     : Anup Jacob, DevOps
 # ----------------------------------
 
-def dictionarytest():
-    return
+def dictionarytest(dict1):
+    keyinput = input("Enter the key: ")
+    value = input("Enter the value: ")
+
+    dict1[keyinput] =value
+
+    choice = input("Do you want to continue adding more key-values to the dictionary (y/n)?")
+    if choice in ('y','Y','yes', 'Yes','YES'):
+        dictionarytest(dict1)
+    elif choice in ('n','N','no', 'No','NO'):
+        print("Ending dictionary")
+    else:
+        print("Wrong choice. Closing addition of dictionary now")
+
+    return dict1
 
 if __name__ == '__main__':
-    print("Dictionary entered is :")
+    dict1 = dict()
+    print("Dictionary entered is :", dictionarytest(dict1))
