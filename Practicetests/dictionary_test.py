f65985bc5231f30f98de094c911fa93ef6164efd
AnupJacob <anupjacob2017@gmail.com>
2025-12-30 14:05:36 +0000

    Initial draft program

---
+++ Practicetests/dictionary_test.py
@@ -0,0 +1,16 @@
+# ---------------------------------
+
+# File          : dictionary_test.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 30/12/2025
+# Modified Date : 30/12/2025
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+def dictionarytest():
+    return
+
+if __name__ == '__main__':
+    print("Dictionary entered is :")
