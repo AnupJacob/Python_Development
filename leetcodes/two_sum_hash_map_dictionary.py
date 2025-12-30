521cb981128dd36b4d84ee0fec71ad289c1623a0
AnupJacob <anupjacob2017@gmail.com>
2025-12-30 17:10:01 +0000

    Initial commit with basic hashmap two test case working

--- leetcodes/two_sum_hash_map_dictionary.py
+++ leetcodes/two_sum_hash_map_dictionary.py
@@ -1,32 +1,74 @@
 # ---------------------------------
 
-# File          : 
+# File          : two_sum_hash_map_dictionary.py
 # Author        : Anup Jacob
 # Version       : v1.0
-# Created Date  :  
-# Modified Date : 
+# Created Date  : 30/12/2025
+# Modified Date : 30/12/2025
 # Description   :
-# Licensing     : Anup Jacob, LYIT
+# Licensing     : Anup Jacob, DevOPs
 # ----------------------------------
 
 '''
 
-   Main method of application 
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
 
 
 
-   Linear programming only presented here wrt demo of lists
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+'''
+
+def hashmaptwosum(numlist, target):
+
+    dict1 = {}
+    result = []
+    print(numlist)
+    print(len(numlist))
+
+    for i in range(0,len(numlist)):
+        if (target - numlist[i]) in numlist:
+            dict1[i] = numlist[i]
+            print("Number: ",numlist[i]," at position : ",i)
+            result.append(i)
+
+    print(result)
+    return dict1
 
 
 
-   Parameters:
+if __name__ == '__main__':
 
-    none
+    nums = [3,2,4]
+    target = 6
 
+    hashmaptwosum(nums,target)
 
-
-   Returns:
-
-    none
-
-  '''   
+    print("The sum leading to the target : ",target," exists.")
