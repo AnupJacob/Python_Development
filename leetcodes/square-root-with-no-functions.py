80a6c490c7351f7fdba6e971376973e4c97b9e68
AnupJacob <anupjacob2017@gmail.com>
2026-01-01 16:17:53 +0000

    Optimised solution for square root

--- leetcodes/square-root-with-no-functions.py
+++ leetcodes/square-root-with-no-functions.py
@@ -6,15 +6,28 @@
 # Created Date  : 01/01/2026
 # Modified Date : 01/01/2026
 # Description   :
-# Licensing     : Anup Jacob, LYIT
+# Licensing     : Anup Jacob, DevOps
 # ----------------------------------
 
-def mySqrt(x):
-    result = round(x ** 0.5)
+#optimised solution:
 
-    return result
+def mySqrt(x):
+    if x == 0 or x == 1:
+        return x
+
+    start = 1
+    end = x
+    while start <= end:
+        mid = (start + end)//2
+        if mid * mid == x:
+            return mid
+        elif mid*mid < x:
+            start = mid + 1
+        elif mid*mid > x:
+            end = mid - 1
+    return end
 
 if __name__ == '__main__':
 
-    x = int(input("Enter the number for square root is to be calculated :"))
+    x = int(input("Enter the number for square root is to be calculated : "))
     print("The square root of the number ",x," is : ",mySqrt(x))
