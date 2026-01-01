a49373f88587e06e2087325950e4d414b8692933
AnupJacob <anupjacob2017@gmail.com>
2026-01-01 16:46:41 +0000

    Initial draft for binary string addition

---
+++ leetcodes/binary-string-addition.py
@@ -0,0 +1,42 @@
+# ---------------------------------
+
+# File          : binary-string-addition.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 01/01/2026
+# Modified Date : 01/01/2026
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+'''
+
+Given two binary strings a and b, return their sum as a binary string.
+
+
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+
+'''
+
+def addBinary(a,b)
+    return
+
+
+if __name__ == '__main__':
+    a = '1010'
+    b = '1011'
+    print("The binary string after addition is : ",addBinary(a,b))
