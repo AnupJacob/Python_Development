187b3e7ee32061427ad3767f74b33753346df2ae
AnupJacob <anupjacob2017@gmail.com>
2026-01-03 11:44:04 +0000

    Initial draft push to github

---
+++ leetcodes/valid-parenthesis.py
@@ -0,0 +1,67 @@
+# ---------------------------------
+
+# File          : valid-prenthesis.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 03/01/2026
+# Modified Date : 03/01/2026
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+'''
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+'''
+
+def isValid():
+    return
+
+
+if __name__ == '__main__':
+    isValid()
