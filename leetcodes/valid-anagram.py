04c9a78d0ca7be8c5ca9793d9b5bcc31b804d971
AnupJacob <anupjacob2017@gmail.com>
2026-01-07 09:44:31 +0000

    Initial draft structure of anagram program

---
+++ leetcodes/valid-anagram.py
@@ -0,0 +1,42 @@
+# ---------------------------------
+
+# File          : valid-anagram.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 07/01/2026
+# Modified Date : 07/01/2026
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+'''
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+'''
+
+
+def isAnagram(s,t):
+
+    return True
+
+
+if __name__ == '__main__':
+    s = "anagram"
+    t = "nagaram"
+
+    print(" The given strings ",s," and ",t," is a ",isAnagram(s,t)," anagram")
