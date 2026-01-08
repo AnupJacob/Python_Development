2d005b2fd1f5ec5e7f9778082777cbc5672fbe26
AnupJacob <anupjacob2017@gmail.com>
2026-01-08 11:59:31 +0000

    Initial draft structure of reverse linked list program

--- leetcodes/reverse-linked-list.py
+++ leetcodes/reverse-linked-list.py
@@ -1,16 +1,63 @@
 # ---------------------------------
 
-# File          : 
+# File          : reverse-linked-list
 # Author        : Anup Jacob
 # Version       : v1.0
-# Created Date  :  
-# Modified Date : 
+# Created Date  : 08/01/2026
+# Modified Date : 08/01/2026
 # Description   :
 # Licensing     : Anup Jacob, DevOps
 # ----------------------------------
 
-def funct()
-    return
-    
+'''
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+'''
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(head):
+        return head
+
+def printlist(head):
+    current = head
+    while current:
+       print(current.val, end=" -> " if current.next else "")
+       current = current.next
+    print(" -> Null")
+
 if __name__ == '__main__':
-    funct()
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(5)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    printlist(node1)
