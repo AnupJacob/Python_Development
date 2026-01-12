8fc4764932a58befbd38dede16dd49cb9ff0b43a
AnupJacob <anupjacob2017@gmail.com>
2026-01-12 11:36:03 +0000

    Initial draft structure of program

---
+++ Practicetests/happy_number_test.py
@@ -0,0 +1,16 @@
+# ---------------------------------
+
+# File          : 
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  :  
+# Modified Date : 
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+def funct():
+    return
+    
+if __name__ == '__main__':
+    funct()
