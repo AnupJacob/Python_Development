e825c333dde28b9cec66ae385e8afdd98d4b1708
AnupJacob <anupjacob2017@gmail.com>
2026-01-12 16:14:57 +0000

    Anagram check using counter

---
+++ Practicetests/anagram_test.py
@@ -0,0 +1,20 @@
+# ---------------------------------
+
+# File          : anagram_test.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 12/01/2026
+# Modified Date : 12/01/2026
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+from collections import Counter
+def isAnagram(s,t):
+    return Counter(s) == Counter(t)
+
+
+if __name__ == '__main__':
+    s = "anagram"
+    t = "nagaram"
+    print("Anagram check for the two strings provided is :",isAnagram(s,t))
