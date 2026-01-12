ed646ca5ca243bd3b5e6eb3dd1432647023eaf86
AnupJacob <anupjacob2017@gmail.com>
2026-01-12 11:53:09 +0000

    Test using list instead of set for checking time space complexity which is O(Log n)

--- Practicetests/happy_number_test.py
+++ Practicetests/happy_number_test.py
@@ -1,16 +1,32 @@
 # ---------------------------------
 
-# File          : 
+# File          : happy_number_test.py
 # Author        : Anup Jacob
 # Version       : v1.0
-# Created Date  :  
-# Modified Date : 
+# Created Date  : 12/01/2026
+# Modified Date : 12/01/2026
 # Description   :
 # Licensing     : Anup Jacob, DevOps
 # ----------------------------------
+#Program written to test if the logic has same time/ space complexity as used by list, where leetcode pgm written using set in my previous solution
+def isHappy(n):
+    nlist = list()
+    strn = str(n)
 
-def funct():
-    return
-    
+    while strn not in nlist:
+        sumn = 0
+        for i in strn:
+            sumn = sumn + pow(int(i),2)
+            print(i)
+            print(sumn)
+
+        if sumn == 1:
+            return True
+        strn = str(sumn)
+
+    return False
+
+
 if __name__ == '__main__':
-    funct()
+    n = 19
+    print("The status of happy number check for the number ",n," is : ",isHappy(n))
