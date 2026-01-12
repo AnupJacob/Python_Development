3d3cf56c6945c0b7a82e59f3be38882336177957
AnupJacob <anupjacob2017@gmail.com>
2026-01-12 15:05:09 +0000

    initial structure for palindrome test program

---
+++ Practicetests/palindrome_test.py
@@ -0,0 +1,18 @@
+# ---------------------------------
+
+# File          : palindorme_test.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 12/01/2026
+# Modified Date : 12/01/2026
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+def isPalindrome(x):
+    return
+
+
+if __name__ == '__main__':
+    x = 121
+    print("The status check for palindrome for the number ",x," is : ",isPalindrome(x))
