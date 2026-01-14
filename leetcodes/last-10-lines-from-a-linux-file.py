e92f823418ac76c749929c20bedd3dd96f2304f2
AnupJacob <anupjacob2017@gmail.com>
2026-01-14 11:51:40 +0000

    Initial program for small files

---
+++ leetcodes/last-10-lines-from-a-linux-file.py
@@ -0,0 +1,23 @@
+# ---------------------------------
+
+# File          : last-10-lines-from-a-linux-file.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 14/01/2026
+# Modified Date : 14/01/2026
+# Description   :
+# Licensing     : Anup Jacob, DevOps
+# ----------------------------------
+
+#For small files
+def print_last_10_lines(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.read().splitlines()
+
+    for line in lines[-10:]:
+        print(line, end=" -> ")
+
+
+if __name__ == '__main__':
+    file_path = "D:\GIT Repo\Git Main\Python_Development\leetcodes\Youtubescriptsample.txt"
+    print_last_10_lines(file_path)
